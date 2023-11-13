0f93e26285312a48eb4acf8d962e1eb73890f30a
Carmel Meelind <cmeelind@tktk.ee>
2023-11-13 10:52:44 +0200

    Week7 task

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const pumpkinData = require('./data/pumpkin.json');
+const broccoliData = require('./data/broccoli.json');
+const rke143Data = require('./data/nodejs.json');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * pumpkinInData.Categories['Pumpkin Drinks'].length);
+    const randomDrinkRecipe = pumpkinInData.Categories['Pumpkin Drinks'][randomIndex];
+    
+    res.status(200).json({randomDrinkRecipe});
+
+});
+
+app.get('/pumpkin', (req, res) => {
+
+    
+
+    res.status(200).json({message: 'Got pumpkin?'});
+});
+
+app.post('/pumpkin', (req, res) => {
+    console.log(req.body);
+
+    if(req.body.username !== 'admin' || req.body.password !== "1234") {
+        res.status(200).json({broccoliData});
+    } else {
+        res.status(200).json({pumpkinData});
+    }
+
+    
+});
+
+app.post('/rke143', (req, res) => {
+    if(req.body.name === 'rke' && req.body.code === "143") {
+        res.status(200).json({rke143Data});
+    } else {
+        res.status(200).json({message: "Invalid credentials"});
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000.');
+});
